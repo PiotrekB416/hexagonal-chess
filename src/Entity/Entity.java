b36ac17edd1c18845214d4929ba45d41e88b3777
PiotrekB416 <piotrek15.pb@gmail..com>
2023-03-31 14:13:48 +0200

    removed unnecasary comments

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,21 +1,3 @@
-//package Entity;
-//
-//import java.util.ArrayList;
-//abstract public class Entity {
-//    protected int rank;
-//    protected String file;
-//    public ArrayList<Object> getPosition(){
-//        ArrayList<Object> list = new ArrayList();
-//        list.add(this.rank);
-//        list.add(this.file);
-//        return list;
-//    }
-//    protected Entity(int rank, String file){
-//        this.rank = rank;
-//        this.file = file;
-//    }
-//}
-
 package Entity;
 
 import java.util.ArrayList;
