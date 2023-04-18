d1bf509eb78cc2984badc742abc59d6f7c762dfd
PiotrekB416 <piotrek15.pb@gmail.com>
2023-04-18 13:18:57 +0200

    merged StartScreen

--- src/App/App.java
+++ src/App/App.java
@@ -5,17 +5,29 @@ import javax.swing.*;
 public class App extends JFrame {
 
     public App(){
-
         super();
-        double scale = 1;
-        super.setSize((int) (1150 * scale), (int)(1200 * scale));
+        super.setTitle("Szachy heksagonalne");
+        super.setIconImage(new ImageIcon("src/Images/brown.png").getImage());
+
+        super.setSize(1150, 1200);
         super.setLocationRelativeTo(null);
         super.setVisible(true);
+        add(new StartScreen());
         super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        Board board = new Board("b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1", 1, -1, this);
-        board.setScale(scale);
-        super.add(board);
-        //super.add(new Board("k/3/5/7/9/1Q9/10Q/Q10/11/11/10K", 1, -1));
-        //super.add(new Board("k/3/5/7/9/2N8/3N2r1n2/10N/6Q4/11/6K4", 1, -1));
+//        super.add(new Board("b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1", true, -1));
+        //super.add(new Board("1/3/5/7/9/2N8/2nN2N4/10N/11/11/11", true, -1));
+    }
+    public App(boolean x){
+        super();
+        super.setTitle("Szachy heksagonalne");
+        super.setIconImage(new ImageIcon("src/Images/brown.png").getImage());
+
+        super.setSize(1150, 1200);
+        super.setLocationRelativeTo(null);
+        super.setVisible(true);
+        super.add(new Board("b/qbk/n1b1n/r5r/ppppppppp/11/5P5/4P1P4/3P1B1P3/2P2B2P2/1PRNQBKNRP1", 1, -1, this));
+
+        super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
 }
---
+++ src/App/StartScreen.java
@@ -0,0 +1,44 @@
+package App;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartScreen extends JPanel {
+    public StartScreen(){
+        setBackground(Color.BLACK);
+        JLabel title = new JLabel("Szachy hexagonalne");
+        title.setFont(new Font("Sans", Font.BOLD, 80));
+        title.setForeground(Color.ORANGE);
+        add(title);
+        setLayout(new GridLayout(5, 1));
+
+        JButton starto = new JButton("START");
+        starto.setFont(new Font("Sans", Font.BOLD, 50));
+        starto.setBackground(Color.BLACK); starto.setForeground(Color.RED); starto.setBorder(null);starto.setFocusPainted(false);
+
+        starto.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new App(true);
+            }
+        });
+
+        add(starto);
+
+        JButton quit = new JButton("Quit");
+        quit.setFont(new Font("Sans", Font.PLAIN, 40));
+        quit.setBackground(Color.BLACK); quit.setForeground(Color.GRAY);quit.setBorder(null);quit.setFocusPainted(false);
+
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(2);
+            }
+        });
+
+        add(quit);
+    }
+}
